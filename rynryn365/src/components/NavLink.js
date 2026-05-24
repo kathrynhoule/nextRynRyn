2b138a7164696e70ac5fee9339ce38a0c18db6a9
kathrynhoule <ryn.houle@gmail.com>
2026-05-24 14:55:04 -0400

    adds temp arrow icon, will change file later

--- rynryn365/src/components/NavLink.js
+++ rynryn365/src/components/NavLink.js
@@ -1,12 +1,18 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function NavLink({ href, children }) {
   return (
+    <div className= "flex flex-row items-center justify-end group">
+      <div className= "hidden group-hover:block">
+        <Image src="/arrow-test-icon.png" alt= "arrow" width={20} height={20} />
+      </div>
     <Link
       href={href}
       className="lowercase"
     >
       {children}
     </Link>
+    </div>
   );
 }
